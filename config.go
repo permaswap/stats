5bd6a55e0f6022e3f5c8894e804ec6ef2aa24d3d
haroldfinch2022 <jayyoung.cn@gmail.com>
2023-01-02 11:55:06 +0800

    improve config

--- config.go
+++ config.go
@@ -11,27 +11,27 @@ func InitPools(chainID int64) (pools map[string]*schema.Pool) {
 
 	case 1: // everPay mainnet
 		eth_usdc := &schema.Pool{
-			"ethereum-eth-0x0000000000000000000000000000000000000000",
-			"ethereum-usdc-0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
-			schema.Fee003,
+			TokenXTag: "ethereum-eth-0x0000000000000000000000000000000000000000",
+			TokenYTag: "ethereum-usdc-0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+			FeeRatio:  schema.Fee003,
 		}
 
 		ar_usdc := &schema.Pool{
-			"arweave,ethereum-ar-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA,0x4fadc7a98f2dc96510e42dd1a74141eeae0c1543",
-			"ethereum-usdc-0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
-			schema.Fee003,
+			TokenXTag: "arweave,ethereum-ar-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA,0x4fadc7a98f2dc96510e42dd1a74141eeae0c1543",
+			TokenYTag: "ethereum-usdc-0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+			FeeRatio:  schema.Fee003,
 		}
 
 		ar_eth := &schema.Pool{
-			"arweave,ethereum-ar-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA,0x4fadc7a98f2dc96510e42dd1a74141eeae0c1543",
-			"ethereum-eth-0x0000000000000000000000000000000000000000",
-			schema.Fee003,
+			TokenXTag: "arweave,ethereum-ar-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA,0x4fadc7a98f2dc96510e42dd1a74141eeae0c1543",
+			TokenYTag: "ethereum-eth-0x0000000000000000000000000000000000000000",
+			FeeRatio:  schema.Fee003,
 		}
 
 		usdc_usdt := &schema.Pool{
-			"ethereum-usdc-0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
-			"ethereum-usdt-0xdac17f958d2ee523a2206206994597c13d831ec7",
-			schema.Fee0005,
+			TokenXTag: "ethereum-usdc-0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+			TokenYTag: "ethereum-usdt-0xdac17f958d2ee523a2206206994597c13d831ec7",
+			FeeRatio:  schema.Fee0005,
 		}
 
 		pools = map[string]*schema.Pool{
@@ -39,20 +39,19 @@ func InitPools(chainID int64) (pools map[string]*schema.Pool) {
 			ar_eth.ID():    ar_eth,
 			usdc_usdt.ID(): usdc_usdt,
 			ar_usdc.ID():   ar_usdc,
-			//ar_cfx.ID():    ar_cfx,
 		}
 
 	case 5:
 		tar_tusdc := &schema.Pool{
-			"bsc-tar-0xf1458ee7e9a2096bce7a21c160840a3a291bcb55",
-			"bsc-tusdc-0xf17a50ecc5fe5f476de2da5481cdd0f0ffef7712",
-			schema.Fee003,
+			TokenXTag: "bsc-tar-0xf1458ee7e9a2096bce7a21c160840a3a291bcb55",
+			TokenYTag: "bsc-tusdc-0xf17a50ecc5fe5f476de2da5481cdd0f0ffef7712",
+			FeeRatio:  schema.Fee003,
 		}
 
 		tar_tardrive := &schema.Pool{
-			"bsc-tar-0xf1458ee7e9a2096bce7a21c160840a3a291bcb55",
-			"bsc-tardrive-0xf4233b165f1b8da4f9aa94abc35c9ad2a7612979",
-			schema.Fee003,
+			TokenXTag: "bsc-tar-0xf1458ee7e9a2096bce7a21c160840a3a291bcb55",
+			TokenYTag: "bsc-tardrive-0xf4233b165f1b8da4f9aa94abc35c9ad2a7612979",
+			FeeRatio:  schema.Fee003,
 		}
 
 		pools = map[string]*schema.Pool{

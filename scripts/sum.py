4e20c079f7b9cdad0494d904f8d8bbe4b49f89fe
haroldfinch2022 <jayyoung.cn@gmail.com>
2023-02-05 17:04:41 +0800

    update sum script

--- scripts/sum.py
+++ scripts/sum.py
@@ -1,5 +1,5 @@
-import datetime, requests
-end = '2022-12-31'
+import datetime, requests, time
+end = '2023-02-05'
 host = 'https://stats.permaswap.network'
 
 def get_stats(host, date):
@@ -7,21 +7,26 @@ def get_stats(host, date):
     return requests.get(url).json()
 
 stats = {}
-start = datetime.date(2022, 12, 22)
+start = datetime.date(2022, 12, 12)
+total = 0
+i = 0
 while True:
     date = start.strftime('%Y-%m-%d')
+    if date == end:
+        break
+    
     data = get_stats(host, date)
     for u, v in data['user'].items():
         stats[u] = stats.get(u, 0) + v
-    
-    if date == end:
-        break
+        total += v
+        i += 1
+
     print(date)
     start = start + datetime.timedelta(days=1)
-
+    time.sleep(0.25)
+    
 stats = dict(sorted(stats.items(), key=lambda item: item[1], reverse=True))
 
+print('total', total, 'i', i)
 for u, v in stats.items():
     print(u, v)
-
-    
